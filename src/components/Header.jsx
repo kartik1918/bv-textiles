4bcd18556fd70da47dac8b666040f9eca1e671cb
Kartik Gupta <kartikgupta966@gmail.com>
2023-03-01 16:33:20 +0530

    feat: initial setup with header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="w-full shadow-lg h-20 m- bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <h1 className="text-3xl font-bold m-auto text-white pt-5">B. V. Textiles</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
